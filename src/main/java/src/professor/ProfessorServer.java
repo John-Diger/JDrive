051e03b8f1c07369766b847e8453ac8f205554a5
JIWEON-JEONG <dlektl6@naver.com>
2022-12-21 14:27:27 +0900

    feat : 소켓 연결

--- src/main/java/src/professor/ProfessorServer.java
+++ src/main/java/src/professor/ProfessorServer.java
@@ -1,25 +1,74 @@
 package src.professor;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.Inet4Address;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 import src.ioagent.InputAgent;
 import src.ioagent.OutputAgent;
 
 public class ProfessorServer {
 
-    private final InputAgent inputAgent;
-    private final OutputAgent outputAgent;
-    private final ProfessorRepository professorRepository;
+    // private final InputAgent inputAgent;
+    // private final OutputAgent outputAgent;
+    // private final ProfessorRepository professorRepository;
 
-    public ProfessorServer(InputAgent inputAgent, OutputAgent outputAgent, ProfessorRepository professorRepository) {
-        this.inputAgent = inputAgent;
-        this.outputAgent = outputAgent;
-        this.professorRepository = professorRepository;
+    ServerSocket serverSocket;
+
+    Socket clientSocket;
+
+    // public ProfessorServer(InputAgent inputAgent, OutputAgent outputAgent, ProfessorRepository professorRepository) {
+    //     this.inputAgent = inputAgent;
+    //     this.outputAgent = outputAgent;
+    //     this.professorRepository = professorRepository;
+    // }
+
+    public ProfessorServer() {
+
     }
 
     // 교수님은 이 메서드를 실행시켜놓으면 학생들의 소켓접근을 허용하고
     // 학생들의 요청/응답에 따라 알맞는 처리를 해준다.
     public static void main(String[] args) {
-
+        ProfessorServer professorServer = new ProfessorServer();
+        professorServer.openServerSocket();
+        professorServer.acceptClientSocket();
+        professorServer.stopServer();
     }
 
+    public void stopServer() {
+        try {
+            clientSocket.close();
+            serverSocket.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
+    public void acceptClientSocket() {
+        while (true) {
+            try {
+                clientSocket = serverSocket.accept();
+                System.out.println("Accept...");
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public void openServerSocket() {
+        try {
+            serverSocket = new ServerSocket();
+            serverSocket.bind(new InetSocketAddress(Inet4Address.getByName("127.0.0.1"), 8080));
+            System.out.println("Listen...");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
--- src/main/java/src/student/StudentClient.java
+++ src/main/java/src/student/StudentClient.java
@@ -1,9 +1,31 @@
 package src.student;
 
+import java.io.IOException;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class StudentClient {
     // 학생들은 이 메서드를 실행시켜 교수님과 소켓 연결을 하고
     // 취하고 싶은 행위를 선택한다.
+
+    Socket clientSocket;
+
     public static void main(String[] args) {
         StudentClient studentClient = new StudentClient();
+        studentClient.connectServerSocket();
+    }
+
+    public void connectServerSocket() {
+        InetAddress inetAddress = null;
+        try {
+            inetAddress = Inet4Address.getByName("127.0.0.1");
+            clientSocket = new Socket(inetAddress, 8080);
+        } catch (UnknownHostException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
